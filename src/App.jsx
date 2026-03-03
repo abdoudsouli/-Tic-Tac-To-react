163ad298d2904f75b3f5abab62238aa4ceae6b9d
dsouli <abdezara90@gmail.com>
2026-03-03 10:57:12 +0000

    add function check win

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,60 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Square from './Components/Square';
 
 
 function App() {
   const [squares,setSquares]=useState(Array(9).fill(null));
-   const [action,setAction]=useState('O');
+   const [action,setAction]=useState('X');
+   const [win,setWin]=useState(null);
   //['null','null'...]
 
   const tic = (i)=>{
     const newSquare = squares.slice()
     if(newSquare[i] !== null){
-      alert('not empty')
+      alert('Alrady checked')
     }else{
-     if (action == 'X') {
-      setAction("O")
-     } else {
-      setAction("X")
-     }
+    setAction(action === 'X' ? 'O' : 'X');
     newSquare[i] = action
     setSquares(newSquare)
+    console.log('tic')
     }
-
   }
+
+ const check_win = () => {
+
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a,b,c] = lines[i];
+
+    if (
+      squares[a] &&
+      squares[a] === squares[b] &&
+      squares[a] === squares[c]
+    ) {
+      setWin(squares[a] + " is Winner");
+    }
+  }
+};
+
+useEffect(() => {
+  check_win()
+}, [squares]);
 
   return (
     <>
   <div className="container">
+    <h2>{win}</h2>
     <h2>Tic Tac Toe</h2>
     <div className="board">
       <Square onSquareClick={()=>tic(0)} value={squares[0]} />
